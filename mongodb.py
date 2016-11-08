17a22e7bdf2477208c98c5c1425954987616f935
Pedro <pedromorfeu@gmail.com>
2016-11-08 23:53:35 +0100

    mongodb

--- mongodb.py
+++ mongodb.py
@@ -9,20 +9,39 @@ print(client)
 db = client.simulation
 print(db)
 
+# db.points.delete_many({})
+
+points = 2 * numpy.random.randn(10000000, 3) + 3
+
+init = 0
+step = 1000000
 for i in range(10):
     print("Iteration", i)
-    points = 2 * numpy.random.randn(1000000, 3) + 3
+    points_sample = points[init:init+step]
+    init += step
     docs = []
-    for pi in points:
+    for pi in points_sample:
         x = {"type": "Point", "coordinates": pi.tolist()}
         docs.append(x)
     print(str(datetime.now()), "Inserting", len(docs), "documents")
     db.points.insert_many(docs)
     print(str(datetime.now()), len(docs), "documents inserted")
 
+
+# MongoDB distance
 print(str(datetime.now()))
 cursor = db.points.find({"coordinates": {"$near": {"$geometry": {"type": "point", "coordinates": [1, 2, 3]}}}}).limit(1)
 print(cursor.next())
 print(str(datetime.now()))
+
+
+# Python distance
+p = (1, 2, 3)
+print(str(datetime.now()))
+distances = numpy.sqrt(((points - p)**2).sum(axis=1))
+sorted_indexes = distances.argsort()
+print(points[sorted_indexes][0])
+print(str(datetime.now()))
+
 
 client.close()
