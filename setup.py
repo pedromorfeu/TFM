cf6c1c1fecec6708b657432b3d0f8aef258d8ca4
Pedro Henrique Mano Fernandes <pedromorfeu@gmail.com>
2016-12-15 18:03:13 +0100

    package creation - setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name = "util",
+    version = "0.0.1",
+    packages = find_packages()
+    )
+# find_packages looks for all packages under the current folder
